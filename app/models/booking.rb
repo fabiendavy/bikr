eae068c1fe73da30dde0d3b4775af4e8aeb9a818
Mathieu Guillermit <mguillermit@gmail.com>
2020-02-17 16:36:18 +0100

    Created bikes seed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,11 +6,11 @@ class Booking < ApplicationRecord
   # validations
   validates :start_date, presence: true
   validates :end_date, presence: true
-  # validates :start_must_be_before_end_time
+  validates :start_must_be_before_end_time if (start_date && end_date)
 
   private
 
   def start_must_be_before_end_time
-    errors.add(:start_time, "must be before end time") unless start_time < end_time
+    errors.add(:start_date, "must be before end time") unless start_date <= end_date
   end
 end
