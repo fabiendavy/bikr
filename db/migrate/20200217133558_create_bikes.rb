ff458068b621bff91a808acd371042aa3d3621a0
Fabien Davy <davyfabien@gmail.com>
2020-02-17 14:38:51 +0100

    generated models

---
+++ db/migrate/20200217133558_create_bikes.rb
@@ -0,0 +1,15 @@
+class CreateBikes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bikes do |t|
+      t.string :type
+      t.string :location
+      t.string :size
+      t.boolean :electric
+      t.string :gender
+      t.text :description
+      t.integer :price_per_day
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_17_133703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bikes", force: :cascade do |t|
+    t.string "type"
+    t.string "location"
+    t.string "size"
+    t.boolean "electric"
+    t.string "gender"
+    t.text "description"
+    t.integer "price_per_day"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total_price"
+    t.bigint "user_id", null: false
+    t.bigint "bike_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bike_id"], name: "index_bookings_on_bike_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.string "gender"
+    t.boolean "owner"
+    t.boolean "client"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "bikes"
+  add_foreign_key "bookings", "users"
 end
