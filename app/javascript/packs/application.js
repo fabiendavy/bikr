3cef4f8666ad65b7c3b362aa10efadcad34a3d2c
Nicolas Lebot <nicolas.lebot@yahoo.fr>
2020-02-20 12:33:59 +0100

    changed bike show layout and added flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,9 +23,11 @@ require("channels")
 
 import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete } from "../plugins/init_autocomplete"
+import { initAutocomplete } from "../plugins/init_autocomplete";
+import { initFlatpickr } from "../plugins/init_flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
   initAutocomplete();
+  initFlatpickr();
 });
