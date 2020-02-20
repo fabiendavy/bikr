413e884edbb1b9ea6de44cd84d1562b3949d637b
Nicolas Lebot <nicolas.lebot@yahoo.fr>
2020-02-20 17:42:15 +0100

    flatpick for dates homepage

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,6 +1,6 @@
 import flatpickr from "flatpickr";
 
-require("flatpickr/dist/themes/material_red.css");
+// require("flatpickr/dist/themes/material_red.css");
 
   const initFlatpickr = () => {
     flatpickr(".datepicker", {});
