7c4a4f582ff157df8807140301df4bca1e6d8db9
Fabien Davy <davyfabien@gmail.com>
2020-02-20 15:55:11 +0100

    perfectionned the home page

--- app/javascript/channels/home.js
+++ app/javascript/channels/home.js
@@ -2,7 +2,7 @@ const setBannerHeight = () => {
   const windowHeight = window.innerHeight;
   const banner = document.querySelector('.banner');
   const navbar = document.querySelector('.navbar');
-  banner.style.height = `${windowHeight - navbar.clientHeight}px`;
+  banner.style.height = `${windowHeight - 58}px`;
 };
 
 export { setBannerHeight }
