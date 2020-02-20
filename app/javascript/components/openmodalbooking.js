b4033c9fcbafc49773daa9d436677ee37669c706
Benjamin Teste <benjamin.teste@gmail.com>
2020-02-20 16:33:03 +0100

    Fix javascript trying to load on every pages

--- app/javascript/components/openmodalbooking.js
+++ app/javascript/components/openmodalbooking.js
@@ -1,13 +1,15 @@
 const booking_button = document.getElementById("booking_button");
 
 const myModal = () => {
-  booking_button.addEventListener("click", () => {
-    const startDate = document.getElementById("booking_start_date").value;
-    const endDate = document.getElementById("booking_end_date").value;
-    console.log(startDate);
-    document.getElementById("booking_modal_body").innerHTML = "Reservation confirmed !";
-    $("#booking_modal").modal("show");
-  });
+  if (booking_button) {
+    booking_button.addEventListener("click", () => {
+      const startDate = document.getElementById("booking_start_date").value;
+      const endDate = document.getElementById("booking_end_date").value;
+      console.log(startDate);
+      document.getElementById("booking_modal_body").innerHTML = "Reservation confirmed !";
+      $("#booking_modal").modal("show");
+    });
+  }
 }
 
 export { myModal };
--- app/javascript/plugins/init_slider.js
+++ app/javascript/plugins/init_slider.js
@@ -1,11 +1,14 @@
-const showSliderValue = () => {
-  var slider = document.getElementById("myRange");
-  var output = document.getElementById("demo");
-  output.innerHTML = slider.value; // Display the default slider value
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
 
-  // Update the current slider value (each time you drag the slider handle)
-  slider.oninput = function() {
-    output.innerHTML = `${this.value} €/day`;
+const showSliderValue = () => {
+  if (slider && output) {
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function() {
+      output.innerHTML = `${this.value} €/day`;
+    }
   }
 }
 
