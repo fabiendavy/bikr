115cacd47c0647be466ec3a07dd0ce9b8a07a9d8
Mathieu Guillermit <mguillermit@gmail.com>
2020-02-20 15:38:24 +0100

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,11 +25,6 @@ import "bootstrap";
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from "../plugins/init_autocomplete"
 import { showSliderValue } from "../plugins/init_slider"
-
-
-
-
-import { initAutocomplete } from "../plugins/init_autocomplete";
 import { initFlatpickr } from "../plugins/init_flatpickr";
 
 document.addEventListener('turbolinks:load', () => {
